9a838a5efe68c23562e6c1c3308a2b898741b252
Lobster Lee <Lobster-Lee@users.noreply.github.com>
2017-05-27 01:27:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,96 @@
 #include <iostream>
-#include <string>
+#include <stack>
+#include <vector>
+#include "math.h"
 using namespace :: std;
 
-struct NumList
+struct ternary
 {
-    char RomeNum;
-    int realNum;
-} List[7] = {
-        'I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000
-    };
+    int bits[21] = {0};
+    int value = 0;
+};
+
+void NumTransform(ternary A[], int m)
+{
+    int i = 0;
+    int n = fabs(A[m].value);
+    int temp;
+    
+    while(n != 0)
+    {
+        temp = n % 3;
+        n = n / 3;
+        A[m].bits[i] = temp;
+        i++;
+    }
+}
 
 int main()
 {
-    int s = 0;
-    int temp, tempCopy;
-    string inputStr;
-    cin >> inputStr;
-    for(int i = 0; i < inputStr.size(); i++)
+    int temp;
+    int Num = 0;
+    stack<int> mystack;
+    int n;
+    cin >> n;
+    ternary A_positive[n];
+    ternary A_negative[n];
+    
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < 7; j++)
-        {
-            if(inputStr[i] == List[j].RomeNum)
-            {
-                temp = List[j].realNum;
-                break;
-            }
-        }
-
-        if(i != 0)
-        {
-            if(temp > tempCopy && (tempCopy == 1 || tempCopy == 10 || tempCopy == 100))
-                s = s + temp - 2 * tempCopy;
-            else
-                s = s + temp;
-        }
-        else if(i == 0)
-            s = s + temp;
-
-        tempCopy = temp;
+        cin >> temp;
+        if(temp >= 0)
+            A_positive[i].value = temp;
+        if(temp < 0 && temp != -2147483648)
+            A_negative[i].value = temp;
+        if(temp == -2147483648)
+            Num++;
     }
-
-    cout << s << endl;
+    
+    for(int i = 0; i < n; i++)
+    {
+        NumTransform(A_positive, i);
+        NumTransform(A_negative, i);
+    }
+    
+    int sum_positive;
+    int sum_negative;
+    bool symbol = 0;
+    for(int i = 0; i < 21; i++)
+    {
+        sum_positive = 0;
+        sum_negative = 0;
+        for(int j = 0; j < n; j++)
+        {
+            sum_positive = sum_positive + A_positive[j].bits[i];
+            sum_negative = sum_negative + A_negative[j].bits[i];
+        }
+        sum_positive = sum_positive % 3;
+        sum_negative = sum_negative % 3;
+        if(sum_positive == 0 && sum_negative != 0)
+            symbol = 1;
+        if(sum_positive != 0 && sum_negative == 0)
+            symbol = 0;
+        mystack.push(fabs(sum_positive + sum_negative));
+    }
+    
+    int i = 20;
+    int result = 0;
+    while(!mystack.empty())
+    {
+        result = result + mystack.top() * (int)pow(3,i);
+        i--;
+        mystack.pop();
+    }
+    
+    if(Num == 1)
+    {
+        cout << -2147483648 << endl;
+    }
+    else if(Num == 3 || Num == 0)
+    {
+        if(symbol)
+            cout << 0 - result << endl;
+        else
+            cout << result << endl;
+    }
 }
